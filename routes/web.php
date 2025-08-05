f7503b63f2dd111f5460b892bc951eb8a96386dc
Nima Heidari <nima.heidari.n@gmail.com>
2025-08-05 20:38:41 +0330

    feat(video): add Route For Create

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\Product;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -23,6 +24,17 @@ Route::get('/product/create', function () {
         'category' => 'Laptop',
         'brand' => 'Apple',
         'price' => '1900',
+    ]);
+});
+
+Route::get('/video/create', function () {
+    return Video::create([
+        'title' => 'Video Episode 1',
+        'url' => 'https://nimaheidari.com/s1/es1/',
+        'description'=>'lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+        'cover'=>'https://nimaheidari.com/s1/es1/cover.jpg',
+        'author'=>'Nima Heidari',
+        'views'=>11,
     ]);
 });
 
