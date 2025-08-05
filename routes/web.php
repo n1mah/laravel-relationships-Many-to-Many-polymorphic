86fc0c02a39a71c6ccbde1ec7a8da989537c179e
Nima Heidari <nima.heidari.n@gmail.com>
2025-08-05 20:32:41 +0330

    feat(post): add Route For Create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/post/create', function () {
+    return Post::create([
+        'title' => 'Laravel 12',
+        'body' => 'This is laravel 12 . lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+        'writer' => 'Nima',
+    ]);
+});
+
